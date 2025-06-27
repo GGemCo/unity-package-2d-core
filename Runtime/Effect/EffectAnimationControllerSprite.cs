c5084802960b0961037c3afa9ed46585923e958a
GGemCo <ggemco.master@gmail.com>
2025-06-27 10:05:24 +0900

    [Effect] 스파인 사용안할지 초기화 문제 수정

--- Runtime/Effect/EffectAnimationControllerSprite.cs
+++ Runtime/Effect/EffectAnimationControllerSprite.cs
@@ -5,6 +5,12 @@ namespace GGemCo2DCore
 {
     public class EffectAnimationControllerSprite : DefaultEffect, IEffectAnimationController
     {
+        private DefaultEffect _defaultEffect;
+        
+        public void Initialize(DefaultEffect defaultEffect)
+        {
+            _defaultEffect =  defaultEffect;
+        }
         public void PlayEffectAnimation(string animationName, bool loop = false, float timeScale = 1, List<StruckAddAnimation> addAnimations = null)
         {
         }
