2f873e7689e457383fefb2a65397c21fb6d717d9
GGemCo <ggemco.master@gmail.com>
2025-08-15 18:11:25 +0900

    [Bug] [Sound] - Runtime 코드에서 Editor 코드를 사용하고 있어, Editor 폴더로 이동

---
+++ Editor/GGemCoTool/Sound/EnumTooltipDrawer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using GGemCo2DCore;
+using UnityEditor;
+using UnityEngine;
+
+namespace GGemCo2DCoreEditor
+{
+    [CustomPropertyDrawer(typeof(SoundConstants.UIButtonType))]
+    public class EnumTooltipDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Type enumType = fieldInfo.FieldType;
+            string[] enumNames = property.enumDisplayNames;
+
+            // Enum 항목마다 Tooltip 가져오기
+            GUIContent[] displayedOptions = new GUIContent[enumNames.Length];
+            var enumValues = Enum.GetValues(enumType);
+
+            for (int i = 0; i < enumNames.Length; i++)
+            {
+                string enumName = enumValues.GetValue(i).ToString();
+                var memberInfo = enumType.GetMember(enumName);
+                string tooltip = "";
+
+                if (memberInfo.Length > 0)
+                {
+                    var tooltipAttr = memberInfo[0].GetCustomAttribute<TooltipAttribute>();
+                    if (tooltipAttr != null)
+                        tooltip = tooltipAttr.tooltip;
+                }
+
+                displayedOptions[i] = new GUIContent(enumNames[i], tooltip);
+            }
+
+            EditorGUI.BeginProperty(position, label, property);
+            property.enumValueIndex = EditorGUI.Popup(position, label, property.enumValueIndex, displayedOptions);
+            EditorGUI.EndProperty();
+        }
+    }
+}
--- Runtime/ScriptableSettings/GGemCoSoundSettings.cs
+++ Runtime/ScriptableSettings/GGemCoSoundSettings.cs
@@ -1,45 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Reflection;
-using UnityEditor;
 using UnityEngine;
 
 namespace GGemCo2DCore
 {
-    [CustomPropertyDrawer(typeof(SoundConstants.UIButtonType))]
-    public class EnumTooltipDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            Type enumType = fieldInfo.FieldType;
-            string[] enumNames = property.enumDisplayNames;
-
-            // Enum 항목마다 Tooltip 가져오기
-            GUIContent[] displayedOptions = new GUIContent[enumNames.Length];
-            var enumValues = Enum.GetValues(enumType);
-
-            for (int i = 0; i < enumNames.Length; i++)
-            {
-                string enumName = enumValues.GetValue(i).ToString();
-                var memberInfo = enumType.GetMember(enumName);
-                string tooltip = "";
-
-                if (memberInfo.Length > 0)
-                {
-                    var tooltipAttr = memberInfo[0].GetCustomAttribute<TooltipAttribute>();
-                    if (tooltipAttr != null)
-                        tooltip = tooltipAttr.tooltip;
-                }
-
-                displayedOptions[i] = new GUIContent(enumNames[i], tooltip);
-            }
-
-            EditorGUI.BeginProperty(position, label, property);
-            property.enumValueIndex = EditorGUI.Popup(position, label, property.enumValueIndex, displayedOptions);
-            EditorGUI.EndProperty();
-        }
-    }
-    
     [CreateAssetMenu(fileName = ConfigScriptableObject.Sound.FileName, menuName = ConfigScriptableObject.Sound.MenuName,
         order = ConfigScriptableObject.Sound.Ordering)]
     public class GGemCoSoundSettings : ScriptableObject
@@ -52,17 +16,6 @@ namespace GGemCo2DCore
         }
         [Tooltip("버튼 Type별로 사운드를 설정합니다.")]
         public List<MappingButtonClickSound> buttonClickSounds;
-        
-        /// <summary>
-        /// 기존 값이 비어있을 때만 기본값을 설정
-        /// </summary>
-        private void OnEnable()
-        {
-            // defaultSoundButtonClick = 0;
-            // defaultSoundButtonClickConfirm = 0;
-            // defaultSoundButtonClickCancel = 0;
-            // defaultSoundButtonClickWindowClose = 0;
-        }
 
         /// <summary>
         /// 처음 생성 시 한 번만 실행됨
