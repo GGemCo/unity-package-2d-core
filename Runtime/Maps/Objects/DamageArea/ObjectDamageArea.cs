d08b7d9efeaa52282e8f21e9a8242e0718da3fa3
GGemCo <ggemco.master@gmail.com>
2025-09-09 11:03:59 +0900

    [Update] [Map/Object] - 캐릭터에게 데미지를 입히는 오브젝트 추가

---
+++ Runtime/Maps/Objects/DamageArea/ObjectDamageArea.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace GGemCo2DCore
+{
+    /// <summary>
+    /// 데미지를 주는 오브젝트
+    /// </summary>
+    public class ObjectDamageArea : DefaultMapObject
+    {
+        public SkillConstants.DamageType damageType;
+        public long damage;
+        public int affectUid;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            CharacterHitArea hitArea = other.GetComponentInChildren<CharacterHitArea>();
+            if (!hitArea) return;
+            // GcLogger.Log($"death zone. OnTriggerEnter2D. damage:{damage}");
+            MetadataDamage metadataDamage = new MetadataDamage
+            {
+                damage = damage,
+                damageType = damageType,
+                affectUid = affectUid
+            };
+            hitArea.target.TakeDamage(metadataDamage);
+        }
+    }
+}
