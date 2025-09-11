5866e1de523953a74ccc9736a554a744759581ab
GGemCo <ggemco.master@gmail.com>
2025-09-11 10:00:55 +0900

    [Update] [Settigs/PlayerAction] - 변경된 수치가 실시간으로 적용될 수 있도록 추가

---
+++ Runtime/ScriptableSettings/ISettingsChangeNotifier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GGemCo2DCore
+{
+    public interface ISettingsChangeNotifier
+    {
+        event Action Changed;
+        void RaiseChanged(); // 필요 시 수동 호출
+    }
+}
