694614fcf2be407a87a807d8dfd0fd00b5a605a3
GGemCo <ggemco.master@gmail.com>
2026-02-27 12:00:48 +0900

    feat(ui-effect): UI 이펙트 제어용 EffectUI 클래스 추가
    한글 설명:
    - UI 상에서 이펙트 오브젝트를 생성 후 제어하기 위한 EffectUI 클래스 신규 추가
    - UI 이펙트의 재생 및 제어를 전담하는 구조 도입
    - 이펙트 속도를 조절할 수 있는 TimeScale 설정 항목 추가
    - UI 연출에 맞춰 이펙트 재생 속도를 유연하게 제어 가능하도록 개선
    
    English description:
    - Added a new EffectUI class to create and control effect objects in UI
    - Introduced a dedicated structure for handling UI effect playback and control
    - Added a TimeScale property to adjust effect playback speed
    - Improved flexibility in controlling UI effect timing and presentation

---
+++ Runtime/Effect/EffectUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGemCo2DCore
+{
+    /// <summary>
+    /// 디폴트 이펙트
+    /// </summary>
+    public class EffectUI : Animation2dController
+    {
+        [Tooltip("재생 속도")]
+        public float timeScale;
+        
+        private float _durationStart;
+        private float _durationPlay;
+        private float _durationEnd;
+        private float _durationTotal;
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            _durationStart = GetAnimationDuration(IEffectAnimationController.KeyClipNameStart, false);
+            _durationPlay = GetAnimationDuration(IEffectAnimationController.KeyClipNamePlay, false);
+            _durationEnd = GetAnimationDuration(IEffectAnimationController.KeyClipNameEnd, false);
+            _durationTotal = _durationStart + _durationPlay + _durationEnd; 
+        }
+
+        private void Start()
+        {
+            List<StruckAddAnimation> newAddAnimations = new List<StruckAddAnimation>();
+            if (_durationPlay > 0)
+            {
+                newAddAnimations.Add(new(IEffectAnimationController.KeyClipNamePlay, true, 0, timeScale));
+            }
+            PlayAnimation(IEffectAnimationController.KeyClipNameStart, false, timeScale, newAddAnimations);
+        }
+
+    }
+}
