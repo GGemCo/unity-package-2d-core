63eb455c1d6454fb405abe6fad8e5a24517023c6
GGemCo <ggemco.master@gmail.com>
2026-04-14 16:22:37 +0900

    feat(scene): SceneManager에 Additive 씬 로드 함수 추가
    한글 설명:
    - SceneManager 클래스에 Additive 방식으로 씬을 로드하는 함수 추가
    - 기존 씬을 유지한 상태에서 새로운 씬을 병합하여 로드 가능하도록 확장
    - 멀티 씬 구성 및 연출 유연성 향상
    
    English description:
    - Added function to SceneManager for loading scenes in additive mode
    - Enabled loading new scenes while keeping existing ones active
    - Improved flexibility for multi-scene setups and scene composition

--- Runtime/Core/SceneManager.cs
+++ Runtime/Core/SceneManager.cs
@@ -1,10 +1,17 @@
-﻿namespace GGemCo2DCore
+﻿using UnityEngine.SceneManagement;
+
+namespace GGemCo2DCore
 {
     public abstract class SceneManager
     {
         public static void ChangeScene(string sceneName)
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+
+        public static void AddScene(string sceneName)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
         }
     }
 }
