9deff06abf30acd0a9bd47cbf51d277736fb6adb
GGemCo <ggemco.master@gmail.com>
2026-05-04 18:27:36 +0900

    feat(map-object-effect): ObjectVfxEffect 클래스 추가 및 맵 이펙트 적용 방식 정의
    한글 설명:
    - 맵에 배치되는 이펙트를 관리하기 위한 ObjectVfxEffect 클래스 추가
    - 맵 배치툴이 아닌 타일맵 오브젝트에 직접 AddComponent 하는 방식으로 적용
    - 특정 오브젝트 기반으로 이펙트를 제어할 수 있도록 구조 확장
    
    English description:
    - Add ObjectVfxEffect class to handle effects placed on the map
    - Apply effects by adding the component directly to tilemap objects instead of using the map placement tool
    - Extend structure to control effects based on specific objects

---
+++ Runtime/Maps/Objects/VfxEffect/ObjectVfxEffect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.U2D;
+
+namespace GGemCo2DCore
+{
+    public class ObjectVfxEffect : DefaultMapObject
+    {
+        [Header(UIWindowConstants.TitleHeaderIndividual)] 
+        [Tooltip("자동 재생 여부")]
+        [SerializeField] private bool autoStart = true;
+        [Tooltip("재생 속도")]
+        [SerializeField] private float timeScale = 1f;
+        [Tooltip("크기")]
+        [SerializeField] private Vector3 scale = Vector3.one;
+        [Tooltip("재생 클립 이름")]
+        [SerializeField] private string animationName;
+        
+        private Animator _animator;
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            _animator = GetComponent<Animator>();
+            InitializeScale();
+        }
+        
+        private void Start()
+        {
+            if (GcLogger.IsNull(_animator, nameof(Animator))) return;
+            
+            _animator.speed = timeScale;
+            _animator.Play(animationName, 0, 0);
+            _animator.Update(0);
+        }
+        
+        private void InitializeScale()
+        {
+            if (scale == Vector3.zero || scale == Vector3.one) return;
+            transform.localScale = scale;
+        }
+    }
+}
